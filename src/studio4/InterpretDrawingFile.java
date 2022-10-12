f667b4fbc2b0e7d47b4752dd318451ddda62edc5
alexandergadin <alexandergadin@localhost>
2022-10-12 17:07:42 -0500

    idk about flags

--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -1,5 +1,6 @@
 package studio4;
 
+import java.awt.Color;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
@@ -15,10 +16,29 @@ import edu.princeton.cs.introcs.StdDraw;
 public class InterpretDrawingFile {
 
 	public static void main(String[] args) throws FileNotFoundException {
-		JFileChooser chooser = new JFileChooser("resources");
-		chooser.showOpenDialog(null);
-		File f = new File(chooser.getSelectedFile().getPath());
-		Scanner in = new Scanner(f); //making Scanner with a File
+//		JFileChooser chooser = new JFileChooser("resources");
+//		chooser.showOpenDialog(null);
+//		File f = new File(chooser.getSelectedFile().getPath());
+//		Scanner in = new Scanner(f); //making Scanner with a File
+		StdDraw.setXscale(0, 12);
+		StdDraw.setYscale(0, 12);
+		StdDraw.setPenColor(Color.YELLOW);
+		StdDraw.filledRectangle(6, 1.5, 6,1.5);
+		StdDraw.setPenColor(Color.BLUE);
+		StdDraw.filledRectangle(6, 4.5, 6,1.5);
+		StdDraw.setPenColor(Color.YELLOW);
+		double[] xs1 = {5,6,7};
+		double[] ys1 = {4,2,4};
+		StdDraw.filledPolygon(xs1,ys1);
+		StdDraw.setPenColor(Color.BLUE);
+		double[] xs2 = {5.5,6,6.5};
+		double[] ys2 = {3,2,3};
+		StdDraw.filledPolygon(xs2,ys2);
+//			Color shapeColor= new Color(1, 1, 0);
+//			StdDraw.setPenColor(shapeColor);t
+//			StdDraw.filledRectangle(1, 1, widthRec,lengthRec);
+		}
+
+
 		
-	}
 }
